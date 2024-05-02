ae7023568e2331f80888abf0543b68e30f4ed75b
PHetem <p_hetem@hotmail.com>
2024-05-02 13:20:57 -0300

    Improve timer logic

--- utils/Timer.py
+++ utils/Timer.py
@@ -4,7 +4,10 @@ import time
 class Timer:
 
     def sleepWithCountdown(seconds, increment, message):
-        while (seconds >= 0):
+        while (seconds > 0):
+            if increment > seconds:
+                increment = seconds
+
             print(message.replace('{SECONDS}', str(seconds)))
             time.sleep(increment)
             seconds -= increment
