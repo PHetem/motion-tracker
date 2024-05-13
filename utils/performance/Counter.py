4fbb92bda3115d5839e0626751bdeef1225d3a20
PHetem <p_hetem@hotmail.com>
2024-05-13 00:38:48 -0300

    Add counter module for performance gauging

---
+++ utils/performance/Counter.py
@@ -0,0 +1,33 @@
+import datetime
+
+class Counter:
+
+    startingTime = 0
+    endingTime = 0
+    lastStepEnd = 0
+    average = 0
+    maxSteps = 0
+    iterations = []
+
+    def start(self, maxSteps = 0):
+        self.maxSteps = maxSteps
+        self.startingTime = datetime.datetime.now().timestamp()
+
+    def end(self):
+        self.endingTime = datetime.datetime.now().timestamp()
+        print('Average per step was ' + str(self.getAverage()))
+        print('Total seconds elapsed was ' + str(self.endingTime - self.startingTime))
+        self.iterations = []
+        self.lastStepEnd = 0
+        self.start(self.maxSteps)
+
+    def addStep(self):
+        lastStepEnd = self.lastStepEnd if self.lastStepEnd > 0 else self.startingTime
+        self.iterations.append(datetime.datetime.now().timestamp() - lastStepEnd)
+
+        self.lastStepEnd = datetime.datetime.now().timestamp()
+        if self.maxSteps > 0 and len(self.iterations) >= self.maxSteps:
+            self.end()
+
+    def getAverage(self):
+        return sum(self.iterations) / len(self.iterations)
